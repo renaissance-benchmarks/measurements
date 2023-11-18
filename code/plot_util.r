f7c31e5f69484afb7651ac095acb4fbc7f2a3356
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2023-11-18 16:29:10 +0100

    Updated results for Renaissance 0.15.0

--- code/plot_util.r
+++
@@ -1,5 +0,0 @@
-#' Write plot to file.
-plot_save <- function (nice, name) {
-    complete <- paste (name, collapse = '-')
-    ggsave (sprintf ('%s.png', complete), nice, width = PLOT_WIDTH, height = PLOT_HEIGHT, unit = 'mm')
-}
